1594e542539fe5d91e5c8e60c4614d54a412d76c
Dan Ober <danober.dev@gmail.com>
2024-08-23 13:47:09 -0500

    nav bar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,18 @@
 // src/components/NavBar.js
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css'; // Ensure you have this file for styling
+import './NavBar.css';
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="navbar">
-      <div className="navbar-content">
         <Link to="/" className="logo">
         <img src="/ober-artisan-logo-large.png" alt="Logo" style={{ width: '200px' }} /> {/* Adjust size as needed */}
-        </Link>
-        <div className="mobile-menu-icon" onClick={toggleMenu}>
-          ☰
-        </div>
-      </div>
-      <div className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
-        <Link to="/shop" onClick={toggleMenu}>Shop</Link>
-        <Link to="/cart" onClick={toggleMenu}>Cart</Link>
-        <Link to="/about" onClick={toggleMenu}>About</Link>
-        <Link to="/contact" onClick={toggleMenu}>Contact</Link>
-
+        </Link><div className="nav-links">
+        <Link to="/shop" className="nav-link">Shop</Link>
+        <Link to="/cart" className="nav-link">Cart</Link>
+        <Link to="/about" className="nav-link">About</Link> 
+        <Link to="/contact" className="nav-link">Contact</Link>
       </div>
     </nav>
   );
