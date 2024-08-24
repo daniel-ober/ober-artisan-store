bec0c76235ec41cbf576f26c0f941dfbe5ecde25
Dan Ober <danober.dev@gmail.com>
2024-08-24 05:40:59 -0500

    tweaks to styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import './About.css'; // Adjust this path to match the location of your CSS file
 const About = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <h1>DO Artisan Drums</h1>
       <p>We are a dedicated team crafting unique and high-quality drums. Our mission is to provide musicians with instruments that inspire creativity and excellence.</p>
     </div>
   );
