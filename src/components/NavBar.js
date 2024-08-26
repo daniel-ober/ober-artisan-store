669d62a7069517f1d5219946dddd84f6816233dd
Dan Ober <danober.dev@gmail.com>
2024-08-25 22:33:56 -0500

    ligin link fixed.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,15 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const NavBar = () => {
   return (
     <nav className="navbar">
-        <Link to="/" className="logo">
+      <Link to="/" className="logo">
         <img src="/ober-artisan-logo-large.png" alt="Logo" style={{ width: '200px' }} /> {/* Adjust size as needed */}
-        </Link><div className="nav-links">
+      </Link>
+      <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/shop" className="nav-link">Shop</Link>
         <Link to="/about" className="nav-link">About</Link> 
         <Link to="/contact" className="nav-link">Contact</Link>
-        <Link to="/signin-email" className="signin-email">Login</Link>
+        <Link to="/signin-email" className="nav-link">Login</Link>
         <Link to="/cart" className="nav-link"><i className="bi bi-cart"></i></Link>
       </div>
     </nav>
