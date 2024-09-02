843f31c9551218d9b781db82a137c0cf7511c015
Dan Ober <danober.dev@gmail.com>
2024-09-02 00:06:26 -0500

    navbar dropdown feature added for mobile and media query.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaCartPlus, FaSignOutAlt, FaUserAlt } from 'react-icons/fa';
+import { FaCartPlus, FaSignOutAlt, FaUserAlt, FaBars } from 'react-icons/fa';
 import './NavBar.css';
 
 const NavBar = ({ isAuthenticated, onSignOut }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => setIsMenuOpen(!isMenuOpen);
+
   const handleSignOut = (event) => {
     event.preventDefault(); // Prevent default link behavior
     onSignOut(); // Call the sign-out function passed as prop
@@ -16,14 +20,17 @@ const NavBar = ({ isAuthenticated, onSignOut }) => {
           <img src="ober-artisan-logo-large.png" alt="Logo" className="logo-img" />
         </Link>
       </div>
-      <div className="navbar-links">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/shop" className="nav-link">Shop/Gallery</Link>
-        <Link to="/about" className="nav-link">About</Link>
-        <Link to="/contact" className="nav-link">Contact</Link>
+      <div className="navbar-menu-toggle" onClick={handleMenuToggle}>
+        <FaBars className="nav-icon" />
+      </div>
+      <div className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>Home</Link>
+        <Link to="/shop" className="nav-link" onClick={() => setIsMenuOpen(false)}>Shop/Gallery</Link>
+        <Link to="/about" className="nav-link" onClick={() => setIsMenuOpen(false)}>About</Link>
+        <Link to="/contact" className="nav-link" onClick={() => setIsMenuOpen(false)}>Contact</Link>
         {isAuthenticated ? (
           <>
-            <Link to="/account" className="nav-link">
+            <Link to="/account" className="nav-link" onClick={() => setIsMenuOpen(false)}>
               <FaUserAlt className="nav-icon" />
               Account
             </Link>
@@ -33,11 +40,11 @@ const NavBar = ({ isAuthenticated, onSignOut }) => {
             </Link>
           </>
         ) : (
-          <Link to="/signin" className="nav-link">
+          <Link to="/signin" className="nav-link" onClick={() => setIsMenuOpen(false)}>
             Sign In
           </Link>
         )}
-        <Link to="/cart" className="nav-link">
+        <Link to="/cart" className="nav-link" onClick={() => setIsMenuOpen(false)}>
           <FaCartPlus className="nav-icon" />
           View Cart
         </Link>
