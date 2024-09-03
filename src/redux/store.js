73f8520e987c7f851ed7286c76c1b544831e0fca
Dan Ober <danober.dev@gmail.com>
2024-09-02 19:48:33 -0500

    cart issue fixed.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 // src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/rootReducer'; // Correct this path
 
-const store = configureStore({
-  reducer: rootReducer,
+import { createStore, combineReducers } from 'redux';
+import cartReducer from './reducers/cartReducer'; // Ensure the path is correct
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  // Add other reducers here if needed
 });
+
+const store = createStore(rootReducer);
 
 export default store;
