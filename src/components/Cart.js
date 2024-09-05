08179e21d136d43115f2da4bbe7ad585b847aadb
Dan Ober <danober.dev@gmail.com>
2024-09-05 18:13:31 -0500

    removed isExpensive var from cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -100,7 +100,6 @@ const Cart = () => {
           cartItems.map(item => {
             const price = Number(item.price) || 0;
             const quantity = Number(item.quantity) || 0;
-            const isExpensive = price >= 500;
             return (
               <div key={item._id} className="cart-item">
                 <Link to={`/item/${item._id}`}>
