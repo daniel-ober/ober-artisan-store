53801877253f08e4185392b9b4401c398362c17a
Dan Ober <danober.dev@gmail.com>
2024-09-07 02:14:22 -0500

    products added to netlify/functions folder

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require('../Product');
+// const Product = require('')
 
 // @route   GET /api/products
 // @desc    Get all products
