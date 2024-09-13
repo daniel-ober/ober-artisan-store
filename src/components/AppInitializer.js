a00198dc6af10e8507523fbbbee62bb8f4215029
Dan Ober <danober.dev@gmail.com>
2024-09-13 16:43:03 -0500

    unique cart ids implemented

---
+++ src/components/AppInitializer.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCart } from '../redux/slices/cartSlice';
+
+const AppInitializer = () => {
+  const dispatch = useDispatch();
+  const userId = 'user123'; // Replace with actual user ID or obtain it dynamically
+  const cartStatus = useSelector((state) => state.cart.status);
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchCart(userId));
+    }
+  }, [dispatch, userId]);
+
+  if (cartStatus === 'loading') {
+    return <div>Loading cart...</div>;
+  }
+
+  return null; // Or some initialization logic
+};
+
+export default AppInitializer;
