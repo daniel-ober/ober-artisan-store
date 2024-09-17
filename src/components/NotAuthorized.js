3860752e4ab46d627f7ff1214945996df25b2c6b
Dan Ober <danober.dev@gmail.com>
2024-09-17 12:46:31 -0500

    admin page basic

---
+++ src/components/NotAuthorized.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotAuthorized = () => {
+  return (
+    <div className="not-authorized">
+      <h1>403 - Not Authorized</h1>
+      <p>You do not have permission to access this page.</p>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default NotAuthorized;
