100644de2e0cfcddc572a7a7f810b9b0f86f6344
Dan Ober <danober.dev@gmail.com>
2024-09-30 00:31:33 -0500

    cart issues fixed. still need to fix issue with adding items from productdetail page.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,93 +7,95 @@ import CartItem from './CartItem';
 import './Cart.css';
 
 const Cart = () => {
-  const { cart, updateQuantity, removeFromCart, loading } = useCart();
-  const { user } = useAuth();
-  const [total, setTotal] = useState(0);
+    const { cart, updateQuantity, removeFromCart, loading } = useCart();
+    const { user } = useAuth();
+    const [total, setTotal] = useState(0);
 
-  // Debugging: Log cart contents
-  console.log('Cart Contents:', cart);
+    // Debugging: Log cart contents
+    console.log('Cart Contents:', cart);
 
-  useEffect(() => {
-    const calculateTotal = () => {
-      const totalAmount = Object.values(cart).reduce((acc, product) => {
-        const price = Number(product.price) || 0;
-        const quantity = Number(product.quantity) || 0;
-        return acc + price * quantity;
-      }, 0);
-      setTotal(totalAmount);
-    };
-
-    calculateTotal();
+    useEffect(() => {
+      const calculateTotal = () => {
+          const totalAmount = Object.values(cart).reduce((acc, product) => {
+              const price = Number(product.price); // Ensure price is a number
+              const quantity = Number(product.quantity); // Ensure quantity is a number
+              const itemTotal = price * quantity; // Calculate item total
+              console.log(`Calculating: ${product.name} - Price: $${price}, Quantity: ${quantity}, Item Total: $${itemTotal}`); // Debugging log
+              return acc + itemTotal; // Update total
+          }, 0);
+          setTotal(totalAmount);
+      };
+  
+      calculateTotal();
   }, [cart]);
 
-  const handleCheckout = async () => {
-    try {
-      const response = await fetch('http://localhost:4949/create-checkout-session', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          products: Object.values(cart).map(product => ({
-            name: product.productId,
-            price: product.priceId,
-            quantity: product.quantity,
-          })),
-          userId: user?.uid,
-        }),
-      });
+    const handleCheckout = async () => {
+        try {
+            const response = await fetch('http://localhost:4949/create-checkout-session', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    products: Object.values(cart).map(product => ({
+                        name: product.productId,
+                        price: product.priceId,
+                        quantity: product.quantity,
+                    })),
+                    userId: user?.uid,
+                }),
+            });
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
 
-      const session = await response.json();
-      window.location.href = session.url;
-    } catch (error) {
-      console.error('Failed to redirect to checkout:', error);
+            const session = await response.json();
+            window.location.href = session.url;
+        } catch (error) {
+            console.error('Failed to redirect to checkout:', error);
+        }
+    };
+
+    if (loading) {
+        return <p>Loading...</p>;
     }
-  };
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+    return (
+        <div className="cart-container">
+            <div className="cart-header">
+                <h2 className="cart-title">Your Shopping Cart</h2>
+                <Link to="/products" className="continue-shopping-link">
+                    <FaArrowLeft className="back-icon" />
+                    Back to Shop/Gallery
+                </Link>
+            </div>
 
-  return (
-    <div className="cart-container">
-      <div className="cart-header">
-        <h2 className="cart-title">Your Shopping Cart</h2>
-        <Link to="/products" className="continue-shopping-link">
-          <FaArrowLeft className="back-icon" />
-          Back to Shop/Gallery
-        </Link>
-      </div>
+            <div className="cart-items">
+                {Object.keys(cart).length > 0 ? (
+                    Object.values(cart).map((product) => (
+                        <CartItem 
+                            key={product.productId} 
+                            item={product} 
+                            updateQuantity={updateQuantity}
+                            removeFromCart={removeFromCart}
+                        />
+                    ))
+                ) : (
+                    <p className="empty-cart-message">Your cart is currently empty.</p>
+                )}
+            </div>
 
-      <div className="cart-items">
-        {Object.keys(cart).length > 0 ? (
-          Object.values(cart).map((product) => (
-            <CartItem 
-              key={product.productId} 
-              item={product} 
-              updateQuantity={updateQuantity}
-              removeFromCart={removeFromCart}
-            />
-          ))
-        ) : (
-          <p className="empty-cart-message">Your cart is currently empty.</p>
-        )}
-      </div>
-
-      <div className="cart-summary">
-        <p className="cart-total">Total: ${total.toFixed(2)}</p>
-        {Object.keys(cart).length > 0 && (
-          <button className="checkout-button" onClick={handleCheckout}>
-            Checkout
-          </button>
-        )}
-      </div>
-    </div>
-  );
+            <div className="cart-summary">
+                <p className="cart-total">Total: ${total.toFixed(2)}</p>
+                {Object.keys(cart).length > 0 && (
+                    <button className="checkout-button" onClick={handleCheckout}>
+                        Checkout
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,124 +6,125 @@ import { FaArrowLeft } from 'react-icons/fa';
 import './ProductDetail.css';
 
 const ProductDetail = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { addToCart, cart } = useCart();
-  const [inCart, setInCart] = useState(null);
-  const [mainImage, setMainImage] = useState('');
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { addToCart, cart } = useCart();
+    const [inCart, setInCart] = useState(null);
+    const [mainImage, setMainImage] = useState('');
 
-  useEffect(() => {
-    const fetchProductData = async () => {
-      try {
-        console.log('Fetching product with ID:', id);
-        const productData = await fetchProductById(id);
-        console.log('Product data:', productData);
+    useEffect(() => {
+        const fetchProductData = async () => {
+            try {
+                console.log('Fetching product with ID:', id);
+                const productData = await fetchProductById(id);
+                console.log('Product data:', productData);
 
-        if (productData) {
-          setProduct(productData);
-          setMainImage(productData.images?.[0] || 'https://i.imgur.com/eoKsILV.png');
+                if (productData) {
+                    setProduct(productData);
+                    setMainImage(productData.images?.[0] || 'https://i.imgur.com/eoKsILV.png');
 
-          // Convert cart object to array for easier handling
-          const cartArray = Object.values(cart);
+                    // Convert cart object to array for easier handling
+                    const cartArray = Object.values(cart);
 
-          // Check if the product is in the cart
-          const cartProduct = cartArray.find(item => item.productId === productData._id);
-          setInCart(cartProduct || null);
-        } else {
-          setError('Product not found');
+                    // Check if the product is in the cart
+                    const cartProduct = cartArray.find(item => item.productId === productData._id);
+                    setInCart(cartProduct || null);
+                } else {
+                    setError('Product not found');
+                }
+            } catch (error) {
+                console.error('Error fetching product:', error);
+                setError('Error fetching product');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProductData();
+    }, [id, cart]);
+
+    const handleAddToCart = () => {
+        if (product) {
+            const quantity = inCart ? inCart.quantity + 1 : 1; // Increment quantity if already in cart
+            addToCart({ ...product, quantity });
         }
-      } catch (error) {
-        console.error('Error fetching product:', error);
-        setError('Error fetching product');
-      } finally {
-        setLoading(false);
-      }
     };
 
-    fetchProductData();
-  }, [id, cart]);
+    const handleQuantityChange = (change) => {
+        if (!inCart) return;
+        const newQuantity = inCart.quantity + change;
 
-  const handleAddToCart = () => {
-    if (product) {
-      addToCart(product);
-    }
-  };
+        if (newQuantity < 1) return;
+        addToCart({ ...product, quantity: newQuantity });
+    };
 
-  const handleQuantityChange = (change) => {
-    if (!inCart) return;
-    const newQuantity = inCart.quantity + change;
+    const handleThumbnailClick = (image) => {
+        setMainImage(image);
+    };
 
-    if (newQuantity < 1) return;
-    addToCart({ ...product, quantity: newQuantity });
-  };
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
 
-  const handleThumbnailClick = (image) => {
-    setMainImage(image);
-  };
+    const canAdjustQuantity = product?.category !== 'custom shop' && product?.category !== 'one of a kind';
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
-
-  const canAdjustQuantity = product?.category !== 'custom shop' && product?.category !== 'one of a kind';
-
-  return (
-    <div className="product-detail-container">
-      <Link to="/products" className="back-to-shop-link">
-        <FaArrowLeft className="back-icon" />
-        Back to Shop/Gallery
-      </Link>
-      <div className="product-image-gallery">
-        <img
-          src={mainImage}
-          alt={product?.name}
-          className="product-main-image"
-        />
-        <div className="product-thumbnail-gallery">
-          {product?.images.map((image, index) => (
-            <button
-              key={index}
-              className="product-thumbnail"
-              onClick={() => handleThumbnailClick(image)}
-            >
-              <img src={image} alt={`Thumbnail ${index + 1}`} />
-            </button>
-          ))}
+    return (
+        <div className="product-detail-container">
+            <Link to="/products" className="back-to-shop-link">
+                <FaArrowLeft className="back-icon" />
+                Back to Shop/Gallery
+            </Link>
+            <div className="product-image-gallery">
+                <img
+                    src={mainImage}
+                    alt={product?.name}
+                    className="product-main-image"
+                />
+                <div className="product-thumbnail-gallery">
+                    {product?.images.map((image, index) => (
+                        <button
+                            key={index}
+                            className="product-thumbnail"
+                            onClick={() => handleThumbnailClick(image)}
+                        >
+                            <img src={image} alt={`Thumbnail ${index + 1}`} />
+                        </button>
+                    ))}
+                </div>
+            </div>
+            <div className="product-info">
+                <h1 className="product-title">{product?.name}</h1>
+                <p className="product-description">{product?.description}</p>
+                <p className="product-price">${product?.price}</p>
+                <button
+                    className="add-to-cart-button"
+                    onClick={handleAddToCart}
+                >
+                    {inCart ? 'Update Cart' : 'Add to Cart'}
+                </button>
+                {canAdjustQuantity && (
+                    <div className="quantity-controls">
+                        <button
+                            className="quantity-button"
+                            onClick={() => handleQuantityChange(-1)}
+                            disabled={!inCart || inCart.quantity <= 1}
+                        >
+                            -
+                        </button>
+                        <span className="quantity-display">{inCart?.quantity || 0}</span>
+                        <button
+                            className="quantity-button"
+                            onClick={() => handleQuantityChange(1)}
+                            disabled={!inCart}
+                        >
+                            +
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-      <div className="product-info">
-        <h1 className="product-title">{product?.name}</h1>
-        <p className="product-description">{product?.description}</p>
-        <p className="product-price">${product?.price}</p>
-        <button
-          className="add-to-cart-button"
-          onClick={handleAddToCart}
-        >
-          {inCart ? 'Update Cart' : 'Add to Cart'}
-        </button>
-        {canAdjustQuantity && (
-          <div className="quantity-controls">
-            <button
-              className="quantity-button"
-              onClick={() => handleQuantityChange(-1)}
-              disabled={!inCart || inCart.quantity <= 1}
-            >
-              -
-            </button>
-            <span className="quantity-display">{inCart?.quantity || 0}</span>
-            <button
-              className="quantity-button"
-              onClick={() => handleQuantityChange(1)}
-              disabled={!inCart}
-            >
-              +
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductDetail;
