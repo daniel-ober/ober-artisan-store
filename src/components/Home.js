a50a30607218c6627c1ad8f200ec4e75d9241e75
Dan Ober <danober.dev@gmail.com>
2024-10-01 14:09:40 -0500

    countdown timer and customized mp4 added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import CountdownTimer from './CountdownTimer';
 import './Home.css';
 
 const Home = () => {
@@ -34,6 +35,23 @@ const Home = () => {
     <div className="home-container">
       <h1 className="welcome-message">Welcome to Dan Ober Artisan Drums!</h1>
       <div className="home-content">
+        <CountdownTimer />
+        {/* Video section */}
+        <div className="video-container">
+          <video
+            className="coming-soon-video"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls
+            onMouseEnter={(e) => e.target.setAttribute('controls', 'controls')}
+            onMouseLeave={(e) => e.target.removeAttribute('controls')}
+          >
+            <source src="https://firebasestorage.googleapis.com/v0/b/danoberartisandrums.appspot.com/o/Home%2Fcomingsoon2.mp4?alt=media&token=31ad46c4-2d4c-447a-821e-13547a75a46f" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
         <div className="home-section">
           <img
             src="https://i.imgur.com/248CyuT.jpeg"
