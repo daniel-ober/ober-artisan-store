248fe3eb22900bb0d726bbfbaef3fc1ee11bd1cd
Dan Ober <danober.dev@gmail.com>
2024-10-01 20:11:02 -0500

    styling enhancements in products, productdetail, productcard, and cart.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,3 @@
-// src/components/ProductCard.js
 import React from 'react';
 import { useCart } from '../context/CartContext';
 import { Link } from 'react-router-dom';
@@ -9,8 +8,7 @@ const ProductCard = ({ product }) => {
 
   const handleAddToCart = () => {
     if (product) {
-      // Adding the product to cart with its ID and other properties
-      addToCart({ ...product, _id: product.id }); // Ensure the product ID is passed correctly
+      addToCart({ ...product, _id: product.id });
     } else {
       console.error('Product is not defined');
     }
@@ -18,27 +16,27 @@ const ProductCard = ({ product }) => {
 
   return (
     <div className="product-card">
-      <img
-        className="product-image"
-        src={product.images?.[0] || 'https://i.imgur.com/eoKsILV.png'}
-        alt={product.name}
-      />
+      <div className="product-image-container">
+        <img
+          className="product-image"
+          src={product.images?.[0] || 'https://i.imgur.com/eoKsILV.png'}
+          alt={product.name}
+        />
+      </div>
       <div className="product-info">
-        <h3 className="product-title">{product.name}</h3>
+        <h3 className="product-title product-title-custom">{product.name}</h3>
         <p className="product-description">{product.description}</p>
         <p className="product-price">${product.price}</p>
-        
         <Link to={`/products/${product.id}`} className="view-details-link">
           View Details
         </Link>
-        
-        <button 
-          className="add-to-cart-button" 
-          onClick={handleAddToCart}
-        >
-          Add to Cart
-        </button>
       </div>
+      <button 
+        className="add-to-cart-button" 
+        onClick={handleAddToCart}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
