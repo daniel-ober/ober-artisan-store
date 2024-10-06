07e3913890716afb6abfed87cf9ab7fc7c879435
Dan Ober <danober.dev@gmail.com>
2024-10-05 23:12:56 -0500

    testing initial mixer idea

---
+++ src/components/AudioPlayer/index.js
@@ -0,0 +1,7 @@
+// src/components/AudioPlayer/index.js
+export { default as MasterFader } from './MasterFader';
+export { default as Mixer } from './Mixer';
+export { default as ResetButton } from './ResetButton';
+export { default as ReverbControl } from './ReverbControl';
+export { default as SignalMeter } from './SignalMeter';
+export { default as Track } from './Track';
