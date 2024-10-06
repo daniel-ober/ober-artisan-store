e2f39b214ea8deaa755bd31c797f1eba7a1f8be7
Dan Ober <danober.dev@gmail.com>
2024-10-05 23:12:56 -0500

    testing initial mixer idea

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebaseConfig';
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { signOut } from 'firebase/auth';
+// import { auth } from '../firebaseConfig';
 
-const SignOut = () => {
-  const navigate = useNavigate();
+// const SignOut = () => {
+//   const navigate = useNavigate();
 
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      navigate('/signin');
-    } catch (error) {
-      console.error('Failed to sign out:', error);
-    }
-  };
+//   const handleSignOut = async () => {
+//     try {
+//       await signOut(auth);
+//       navigate('/signin');
+//     } catch (error) {
+//       console.error('Failed to sign out:', error);
+//     }
+//   };
 
-  return <button onClick={handleSignOut}>Sign Out</button>;
-};
+//   return <button onClick={handleSignOut}>Sign Out</button>;
+// };
 
-export default SignOut;
+// export default SignOut;
