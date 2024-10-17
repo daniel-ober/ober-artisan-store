4c0b07b8d9c9d240967d7655c76c1272dd3e40fe
Dan Ober <danober.dev@gmail.com>
2024-10-16 20:29:38 -0500

    admin db fixes through sdk

--- src/App.js
+++ src/App.js
@@ -1,90 +1,76 @@
-// src/App.js
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';
-import About from './components/About';
-import AccountPage from './components/AccountPage';
-import AdminDashboard from './components/AdminDashboard';
-import Cart from './components/Cart';
-import ChatSupportButton from './components/ChatSupportButton';
-import Checkout from './components/Checkout';
-import Contact from './components/Contact';
-import CustomShopAssistant from './components/CustomShopAssistant';
-import Footer from './components/Footer';
-import ForgotPassword from './components/ForgotPassword';
-import Home from './components/Home';
-import ManageOrders from './components/ManageOrders';
-import ManageProducts from './components/ManageProducts';
-import ManageUsers from './components/ManageUsers';
 import NavBar from './components/NavBar';
-import NotAuthorized from './components/NotAuthorized';
-import NotFound from './components/NotFound';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import PrivateRoute from './components/PrivateRoute';
-import ProductDetail from './components/ProductDetail';
+import Home from './components/Home';
 import Products from './components/Products';
-import Register from './components/Register';
-import ReturnPolicy from './components/ReturnPolicy';
+import About from './components/About';
+import Contact from './components/Contact';
+import Cart from './components/Cart';
 import SignInEmail from './components/SignInEmail';
+import Register from './components/Register'; // Add Register component
+import ForgotPassword from './components/ForgotPassword'; // Add ForgotPassword component
+import Checkout from './components/Checkout';
+import ProductDetail from './components/ProductDetail';
+import AccountPage from './components/AccountPage';
+import TransactionSuccess from './components/TransactionSuccess'; // Adjust the import as necessary
+import AdminDashboard from './components/AdminDashboard';
+import ManageUsers from './components/ManageUsers';
+import ManageProducts from './components/ManageProducts';
+import ManageOrders from './components/ManageOrders';
 import SiteSettings from './components/SiteSettings';
+import CustomShopAssistant from './components/CustomShopAssistant';
+import PrivacyPolicy from './components/PrivacyPolicy';
 import TermsOfService from './components/TermsOfService';
-import TransactionSuccess from './components/TransactionSuccess';
-import * as AudioPlayer from './components/AudioPlayer'; // Import everything from index.js
-import * as firebaseService from './services/firebaseService'; // Import Firebase service functions
+import ReturnPolicy from './components/ReturnPolicy';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+import NotAuthorized from './components/NotAuthorized';
+import PrivateRoute from './components/PrivateRoute';
+import { useAuth } from './context/AuthContext';
+import ChatSupportButton from './components/ChatSupportButton';
 
 function App() {
-    const { user, handleSignOut } = useAuth();
-    const [currentTab, setCurrentTab] = useState('Home');
+  const { user, handleSignOut } = useAuth();
+  const [currentTab, setCurrentTab] = useState('Home');
 
-    // Expose firebaseService functions globally
-    window.firebaseService = firebaseService;
-
-    return (
-        <div className="app-container">
-            <NavBar 
-                isAuthenticated={!!user} 
-                onSignOut={handleSignOut} 
-                onTabChange={(tab) => setCurrentTab(tab)} 
-            />
-            <div className="app-content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/account" element={<PrivateRoute element={<AccountPage />} />} />
-                    
-
-                    <Route path="/admin" element={<AdminDashboard />} />
-                    <Route path="/admin/users" element={<ManageUsers />} />
-                    <Route path="/admin/products" element={<ManageProducts />} />
-                    <Route path="/admin/orders" element={<ManageOrders />} />
-                    <Route path="/admin/settings" element={<SiteSettings />} />
-                        
-                    {/* <Route path="/admin" element={<PrivateRoute element={<AdminDashboard />} adminOnly />} />
-                    <Route path="/admin/users" element={<PrivateRoute element={<ManageUsers />} adminOnly />} />
-                    <Route path="/admin/products" element={<PrivateRoute element={<ManageProducts />} adminOnly />} />
-                    <Route path="/admin/orders" element={<PrivateRoute element={<ManageOrders />} adminOnly />} />
-                    <Route path="/admin/settings" element={<PrivateRoute element={<SiteSettings />} adminOnly />} /> */}
-                    <Route path="/about" element={<About />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/checkout" element={<PrivateRoute element={<Checkout />} />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/custom-shop-assistant" element={<PrivateRoute element={<CustomShopAssistant />} adminOnly />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/mixing-booth" element={<PrivateRoute element={<AudioPlayer.Mixer />} adminOnly />} />
-                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                    <Route path="/products" element={<Products />} />
-                    <Route path="/products/:id" element={<ProductDetail />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/return-policy" element={<ReturnPolicy />} />
-                    <Route path="/signin" element={<SignInEmail />} />
-                    <Route path="/success" element={<TransactionSuccess />} />
-                    <Route path="/terms-of-service" element={<TermsOfService />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-            <ChatSupportButton currentTab={currentTab} /> {/* Pass currentTab here */}
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="app-container">
+      <NavBar 
+        isAuthenticated={!!user} 
+        onSignOut={handleSignOut} 
+        onTabChange={(tab) => setCurrentTab(tab)} 
+      />
+      <div className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/custom-shop-assistant" element={<CustomShopAssistant />} />
+          <Route path="/signin" element={<SignInEmail />} />
+          {/* <Route path='/not-authorized' element={<NotAuthorized />} /> */}
+          <Route path="/register" element={<Register />} /> {/* Add register route */}
+          <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Add forgot password route */}
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/success" element={<TransactionSuccess />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/return-policy" element={<ReturnPolicy />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/checkout" element={<PrivateRoute element={<Checkout />} />} />
+          <Route path="/account" element={<PrivateRoute element={<AccountPage />} />} />
+          <Route path="/admin" element={<PrivateRoute element={<AdminDashboard />} adminOnly />} />
+          <Route path="/admin/users" element={<PrivateRoute element={<ManageUsers />} adminOnly />} />
+          <Route path="/admin/products" element={<PrivateRoute element={<ManageProducts />} adminOnly />} />
+          <Route path="/admin/orders" element={<PrivateRoute element={<ManageOrders />} adminOnly />} />
+          <Route path="/admin/settings" element={<PrivateRoute element={<SiteSettings />} adminOnly />} />
+        </Routes>
+      </div>
+      <ChatSupportButton currentTab={currentTab} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,5 @@
+// src/context/AuthContext.js
+
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
