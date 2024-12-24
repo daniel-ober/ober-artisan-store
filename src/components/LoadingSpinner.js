54d12da5d64392bde273ea4adf64832559c58732
Dan Ober <danober.dev@gmail.com>
2024-12-24 10:56:53 -0600

    enhanced create product modal

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+      <p>Creating your product...</p>
+    </div>
+  );
+  
+  export default LoadingSpinner;
+  
