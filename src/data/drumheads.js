a9dc7db110af79fd848fe1ed89b50237427d4866
Dan Ober <danober.dev@gmail.com>
2024-12-30 12:06:18 -0600

    added preset data for spider tool.

--- src/data/drumheads.js
+++ src/data/drumheads.js
@@ -1,54 +1,56 @@
+// drumheadTypes.js
+
 const drumheads = [
-    {
-      drumhead: 'Coated',
-      soundProfile: {
-        attack: 7,
-        sustain: 8,
-        warmth: 9,
-        projection: 7,
-        brightness: 5,
-      },
+  {
+    drumhead: 'Coated',
+    soundProfile: {
+      attack: 7,
+      sustain: 8,
+      warmth: 9,
+      projection: 7,
+      brightness: 5,
     },
-    {
-      drumhead: 'Clear',
-      soundProfile: {
-        attack: 8,
-        sustain: 6,
-        warmth: 5,
-        projection: 9,
-        brightness: 7,
-      },
+  },
+  {
+    drumhead: 'Clear',
+    soundProfile: {
+      attack: 8,
+      sustain: 6,
+      warmth: 5,
+      projection: 9,
+      brightness: 7,
     },
-    {
-      drumhead: 'Mesh',
-      soundProfile: {
-        attack: 6,
-        sustain: 7,
-        warmth: 4,
-        projection: 5,
-        brightness: 6,
-      },
+  },
+  {
+    drumhead: 'Mesh',
+    soundProfile: {
+      attack: 6,
+      sustain: 7,
+      warmth: 4,
+      projection: 5,
+      brightness: 6,
     },
-    {
-      drumhead: 'Felt (For Muffling)',
-      soundProfile: {
-        attack: 4,
-        sustain: 3,
-        warmth: 8,
-        projection: 2,
-        brightness: 2,
-      },
+  },
+  {
+    drumhead: 'Felt (For Muffling)',
+    soundProfile: {
+      attack: 4,
+      sustain: 3,
+      warmth: 8,
+      projection: 2,
+      brightness: 2,
     },
-    {
-      drumhead: 'Hybrid (Coated + Clear)',
-      soundProfile: {
-        attack: 8,
-        sustain: 7,
-        warmth: 6,
-        projection: 8,
-        brightness: 6,
-      },
+  },
+  {
+    drumhead: 'Hybrid (Coated + Clear)',
+    soundProfile: {
+      attack: 8,
+      sustain: 7,
+      warmth: 6,
+      projection: 8,
+      brightness: 6,
     },
-  ];
-  
-  export default drumheads;
+  },
+];
+
+export default drumheads;
