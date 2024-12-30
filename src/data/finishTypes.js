4322c6bd8898d959faced46ce058ef69a57b49df
Dan Ober <danober.dev@gmail.com>
2024-12-30 11:30:14 -0600

    added data for soundProfiles.

---
+++ src/data/finishTypes.js
@@ -0,0 +1,64 @@
+const finishTypes = [
+    {
+      finish: 'Glossy',
+      soundProfile: {
+        attack: 7,
+        sustain: 6,
+        warmth: 6,
+        projection: 7,
+        brightness: 8,
+      },
+    },
+    {
+      finish: 'Matte',
+      soundProfile: {
+        attack: 6,
+        sustain: 7,
+        warmth: 8,
+        projection: 6,
+        brightness: 5,
+      },
+    },
+    {
+      finish: 'Satin',
+      soundProfile: {
+        attack: 7,
+        sustain: 6,
+        warmth: 7,
+        projection: 6,
+        brightness: 6,
+      },
+    },
+    {
+      finish: 'High Gloss',
+      soundProfile: {
+        attack: 8,
+        sustain: 6,
+        warmth: 5,
+        projection: 8,
+        brightness: 9,
+      },
+    },
+    {
+      finish: 'Lacquered',
+      soundProfile: {
+        attack: 7,
+        sustain: 7,
+        warmth: 6,
+        projection: 7,
+        brightness: 7,
+      },
+    },
+    {
+      finish: 'Scorched/Toasted',
+      soundProfile: {
+        attack: 7,
+        sustain: 8,
+        warmth: 9,
+        projection: 7,
+        brightness: 4,
+      },
+    },
+  ];
+  
+  export default finishTypes;
