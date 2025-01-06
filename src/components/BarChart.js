7a9d02db7c895071278cecbb2395ea499384999c
Dan Ober <danober.dev@gmail.com>
2025-01-06 08:19:12 -0600

    custom drum builder randomizer tool added.

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './BarChart.css';
 
-const BarChart = ({ data }) => {
+const BarChart = ({ data, min = 5 }) => {
   return (
     <div className="bar-chart">
-      {Object.entries(data).map(([key, value]) => (
-        <div key={key} className="bar-container">
-          <div className="bar-label">{key.toUpperCase()}</div>
-          <div className="bar">
-            <div
-              className={`bar-fill ${key}`}
-              style={{ width: `${value * 10}%` }}  // Scale to 100
-            ></div>
-          </div>
-        </div>
-      ))}
+      {Object.entries(data)
+        .filter(([key, value]) => value >= min)  // Filter out values below min
+        .map(([key, value]) => {
+          const percentage = (value / 10) * 100;  // Scale to 100 based on max 10
+
+          return (
+            <div key={key} className="bar-container">
+              <div className="bar-label">{key.toUpperCase()}</div>
+              <div className="bar">
+                <div
+                  className={`bar-fill ${key}`}
+                  style={{
+                    width: `${percentage}%`
+                  }}
+                ></div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
