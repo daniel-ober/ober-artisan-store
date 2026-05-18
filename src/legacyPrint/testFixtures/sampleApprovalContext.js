4751e60ab358bbe13fce582022de5f118d07acda
Dan Ober <danober.dev@gmail.com>
2026-05-18 02:29:17 -0500

    Add LegacyPrint research patch approval workflow

---
+++ src/legacyPrint/testFixtures/sampleApprovalContext.js
@@ -0,0 +1,15 @@
+import { sampleDrumReference } from './sampleDrumReference.js';
+
+import { sampleResearchPatch } from './sampleResearchPatch.js';
+
+export const sampleApprovalContext = {
+
+  baseDocument: sampleDrumReference,
+
+  researchPatch: sampleResearchPatch,
+
+  performedBy: 'danober',
+
+  reason: 'Validated shell thickness research update'
+
+};
