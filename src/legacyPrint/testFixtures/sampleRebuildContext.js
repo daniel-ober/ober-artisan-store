82d806ccfdd974618613fb0e39dbee094f0fff50
Dan Ober <danober.dev@gmail.com>
2026-05-18 02:32:03 -0500

    Add LegacyPrint rebuild queue entry generator

---
+++ src/legacyPrint/testFixtures/sampleRebuildContext.js
@@ -0,0 +1,15 @@
+import { sampleDrumReference } from './sampleDrumReference.js';
+
+export const sampleRebuildContext = {
+
+  targetDrumReferenceId: sampleDrumReference.id,
+
+  rebuildReason: 'canonicalReferenceUpdated',
+
+  engineVersion: '0.1.0',
+
+  calibrationProfileId: 'snare_default_v1',
+
+  requestedBy: 'danober'
+
+};
